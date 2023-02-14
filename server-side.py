270b4b31f5142e18872806bf07610cb269afd801
daniel.dobson <daniel.dobson@FF-LAPTOP-DDN.local>
2023-02-14 16:39:50 +0100

    alle data vezameld server side

--- server-side.py
+++ server-side.py
@@ -2,34 +2,45 @@ import socket
 import sys
 import requests
 import json
+import plotly
 
 request_url = "https://www.mediawiki.org/w/api.php?action=query&format=json&prop=revisions&list=allrevisions&meta=siteinfo&continue=&titles=Main%20Page&formatversion=2&rvprop=user%7Ccomment&arvlimit=50&arvdir=newer"
 
 response = requests.get(request_url)
 response_json = response.json() # This method is convenient when the API returns JSON
 json_formatted_str = json.dumps(response_json, indent=2)
-print(json_formatted_str)
+
+revisions = []
+
+class Revision:
+    def __init__(self, timestamps: list, users: list, comments: list):
+        self.timestamps = timestamps
+        self.users = users
+        self.comments = comments
 
 
-def server():
-    host = socket.gethostname()
-    port = 7000
-    server_socket = socket.socket()
-    server_socket.bind((host, port))
-    server_socket.listen(1)
-    connection, address = server_socket.accept()
-    print("Connection from: " + str(address))
-    while True:
-        data = connection.recv(1024).decode()
-        if not data:
-            break
-        print("from connected user: " + str(data))
-        connection.send(data.encode())
-    connection.close()
+def parse_revisions():
 
+    for page in enumerate(response_json["query"]["allrevisions"]):
+        print("Page: ", page[1]["title"])
+        
+        count = 0
 
-def wikipedia_api_GET_request():
-    pass
+        timestamps = []
+        users = []
+        comments = []
 
-# if __name__ == "__main__":
-#     server()
+        for rev in page[1]["revisions"]:
+            count += 1
+            timestamps.append(rev["timestamp"])
+            users.append(rev["user"])
+            comments.append("comment")
+
+        print("Number of revisions: ", count, "\n")
+    
+        revision = Revision(timestamps, users, comments)
+        revisions.append(revision)
+
+parse_revisions()
+
+print("Number of total revisions: ", len(revisions))
