99c75db612722266b707e218401d785c1a1e6874
daniel.dobson <daniel.dobson@FF-LAPTOP-DDN.local>
2023-02-14 14:51:12 +0100

    added GET response request

--- server-side.py
+++ server-side.py
@@ -1,7 +1,15 @@
 import socket
 import sys
+import requests
+import json
 
 request_url = "https://www.mediawiki.org/w/api.php?action=query&format=json&prop=revisions&list=allrevisions&meta=siteinfo&continue=&titles=Main%20Page&formatversion=2&rvprop=user%7Ccomment&arvlimit=50&arvdir=newer"
+
+response = requests.get(request_url)
+response_json = response.json() # This method is convenient when the API returns JSON
+json_formatted_str = json.dumps(response_json, indent=2)
+print(json_formatted_str)
+
 
 def server():
     host = socket.gethostname()
@@ -23,5 +31,5 @@ def server():
 def wikipedia_api_GET_request():
     pass
 
-if __name__ == "__main__":
-    server()
+# if __name__ == "__main__":
+#     server()
